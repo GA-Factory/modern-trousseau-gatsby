5bfc8d001ea07e542e9b403ad43049147685d225
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-05 16:30:25 -0500

    component set up, starting build

---
+++ src/components/component-collection/ComponentCollection.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const ComponentCollection = () => (
+    <>
+        <footer>
+            <p>dfhjdk</p>
+        </footer>
+    </>
+)
+
+
+export default ComponentCollection
---
+++ src/components/component-collection/ComponentCollection.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import ComponentCollection  from './ComponentCollection';
+
+storiesOf("Component-Collection", module).add("Component-Collection", () => (
+  <ComponentCollection title="Fall 2020 Collection"></ComponentCollection>
+));
