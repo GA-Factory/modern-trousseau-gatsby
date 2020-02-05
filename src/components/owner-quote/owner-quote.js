6b8cbb6c01cecc9ac592a8d89965ce9fce647d3c
Adam_Bates <Creizai@EMAIL.COM>
2020-02-05 16:21:47 -0500

    Started styling the component still needs proper font

--- src/components/owner-quote/owner-quote.js
+++ src/components/owner-quote/owner-quote.js
@@ -2,16 +2,43 @@
 import React from "react";
 import ownerPicture from "../../assets/images/owner-quote-callie-photo.png";
 
+import styled from "styled-components";
+
+const OwnerCard = styled.div`
+  display: flex;
+`;
+
+const OwnerQuoteSection = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  justify-content: space-between;
+  align-items: center;
+  width: 420px;
+  text-align: center;
+`;
+
+const OwnerFontWriting = styled.h1`
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 30px;
+  line-height: 40px;
+  text-align: center;
+
+  color: #000000;
+`;
 // This is a functional component - just sent up a little differently as an arrow function!
 const OwnerQuote = props => (
-  <div>
+  <OwnerCard>
     <img src={ownerPicture} alt="callie" />
-    <p>
-      "The look on a bride's face when she finds her dress - that is priceless
-      and incredibly rewarding."
-    </p>
-    <p>Callie Tein | Designer</p>
-  </div>
+    <OwnerQuoteSection>
+      <h1>
+        "The look on a bride's face when she finds her dress - that is priceless
+        and incredibly rewarding."
+      </h1>
+      <h1>Callie Tein | Designer</h1>
+    </OwnerQuoteSection>
+  </OwnerCard>
 );
 
 export default OwnerQuote;
