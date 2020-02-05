4342792416638529b17f15386d73986fd90364bf
“Erin <erinkelley27@gmail.com>
2020-02-05 10:20:32 -0500

    added gatsby storybook starter code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,31 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Gatsby Storybook Starter',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'Gatsby Storybook Starter',
+        short_name: 'Gatsby Storybook Starter',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#744C9E',
+        display: 'standalone',
+        icon: 'src/assets/logos/icon.png',
+      },
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/`,
+        name: 'src',
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-root-import',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-styled-components',
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,41 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import styled from 'styled-components';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../global/Layout';
+import Image from '../components/Image';
+
+const Page = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Heading = styled.h1`
+  font-size: 24px;
+  color: #555;
+  margin-top: 60px;
+`;
+
+const Label = styled.p`
+  font-size: 14px;
+  color: #aaa;
+  margin-top: 12px;
+  letter-spacing: 10px;
+  text-transform: uppercase;
+`;
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <Page>
       <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+      <Heading>GatsbyJS + Storybook</Heading>
+      <Label>Starter</Label>
+    </Page>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
