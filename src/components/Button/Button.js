8e7bec23e54d3bcc9bcf94c02aadfb17003fb7ab
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-02-06 13:14:48 -0500

    button's styled

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,21 +1,32 @@
 import React from "react";
 import "./Button.css";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 const Button = styled.button`
-  background-color: #fae5dc;
-  color: ${props => props.ftcolor};
-  font-weight: ${props => props.ftweight};
-  box-shadow: ${props => props.ftshadow};
-  padding: ${props => props.ftpadding};
-  width: ${props => props.ftwidth};
-  font-size: ${props => props.ftsize};
-  line-height: ${props => props.ftheight};
-  
+  background-color: ${props => props.theme.colors.primaryPink};
+  color: ${props => props.color};
+  font-family: ${props => props.family};
+  text-align: center;
+  font-weight: ${props => props.weight};
+  box-shadow: ${props => props.shadow};
+  padding: ${props => props.padding};
+  width: ${props => props.width};
+  font-size: ${props => props.fontsize};
+  line-height: ${props => props.height};
+
+  @media (min-width: ${props => props.theme.breakpoints.laptop}) {
+    font-family: 'Raleway';
+    padding: ${props => props.desktoppadding};
+    width: ${props => props.desktopwidth};
+    font-weight: ${props => props.desktopweight};
+    font-size: ${props => props.desktopfontsize};
+    line-height: ${props => props.desktopheight};
+    color: ${props => props.desktopcolor}
+  }
 `
 
 const PinkButton = props => {
-  return <Button>{props.label}</Button>;
+  return <Button weight={props.weight} family={props.family} fontsize={props.fontsize} height={props.height} padding={props.padding} width={props.width} shadow={props.shadow} desktopwidth={props.desktopwidth} desktoppadding={props.desktoppadding} desktopheight={props.desktopheight} desktopfontsize={props.desktopfontsize} desktopweight={props.desktopweight} desktopcolor={props.desktopcolor} color={props.color}>{props.label}</Button>;
 };
 
 export default PinkButton;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,11 +3,15 @@ import { storiesOf } from "@storybook/react";
 import PinkButton from "./Button";
 
 storiesOf("Button", module)
-  .add("Browse Now", () => <PinkButton label="Browse Now" ftcolor="#441f0e" ftweight="bold" ftsize="15px" ftheight="23px" />)
-  .add("Book Now", () => <PinkButton label="Book Now" type="light-pink" />)
-  .add("Visit Us", () => <PinkButton label="Visit Us" type="light-pink" />)
-  .add("View Dress", () => <PinkButton label="View Dress" type="light-pink" />)
-  .add("Submit", () => <PinkButton label="SUBMIT" type="light-pink" />)
+  .add("Browse Now", (props) => <PinkButton label="Browse Now" fontsize={`${"14px"}`} weight={`${"bold"}`} height={`${"22px"}`} padding={`${"5px 0"}`} width={`${"88px"}`} desktopwidth={`${"126px"}`} family={`${"'Playfair Display'"}`} color={`${"#441F0E"}`} />)
+
+  .add("Book Now", () => <PinkButton label="Book Now" fontsize={`${"14px"}`} weight={`${"bold"}`} height={`${"22px"}`} padding={`${"5px 0"}`} width={`${"88px"}`} desktopwidth={`${"126px"}`} family={`${"'Playfair Display'"}`} color={`${"#441F0E"}`} />)
+
+  .add("Visit Us", () => <PinkButton label="Visit Us" fontsize={`${"14px"}`} weight={`${"bold"}`} height={`${"22px"}`} padding={`${"5px 0"}`} width={`${"88px"}`} desktopwidth={`${"126px"}`} family={`${"'Playfair Display'"}`} />)
+
+  .add("View Dress", () => <PinkButton label="View Dress" fontsize={`${"14px"}`} weight={`${"bold"}`} height={`${"22px"}`} width={`${"88px"}`} padding={`${"5px 0"}`} shadow={`${"0px 4px 4px rgba(0, 0, 0, 0.25)"}`} desktopwidth={`${"83px"}`} desktoppadding={`${"5px"}`} desktopheight={`${"14px"}`}  desktopfontsize={`${"12px"}`} desktopweight={`${"600"}`} desktopcolor={`${"#000000"}`} family={`${"'Playfair Display'"}`} color={`${"#441F0E"}`}/>)
+
+  .add("Submit", () => <PinkButton label="SUBMIT" width={`${"63px"}`} fontsize={`${"12px"}`} height={`${"14px"}`} weight={`${"300"}`} padding={`${"5px 0"}`} shadow={`${"0px 4px 4px rgba(0, 0, 0, 0.25)"}`} color={`${"#000000"}`} desktopfontsize={`${"14px"}`} desktopheight={`${"16px"}`} desktopwidth={`${"79px"}`} />)
 
 
 
