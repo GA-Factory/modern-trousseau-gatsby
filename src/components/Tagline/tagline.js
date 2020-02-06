4bd64551cd388796264bd591c1577c68d589e85c
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-02-06 16:33:58 -0500

    desktop styling finished

--- src/components/Tagline/tagline.js
+++ src/components/Tagline/tagline.js
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: space-around;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: space-around;
+  text-align: center;
+`;
 
 const Headline = styled.h1`
   font-family: "Playfair Display";
@@ -15,25 +16,33 @@ const Headline = styled.h1`
   font-size: 18px;
   line-height: 24px;
   text-align: center;
-  color: #0F1F2F;
+  color: #0f1f2f;
+
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    ${props => props.theme.fontStyles.h1Styles}
+  }
 `;
 
 const Slogan = styled.div`
-font-family: 'Raleway';
-font-style: normal;
-font-weight: normal;
-font-size: 11px;
-line-height: 13px;
-color: #0F1F2F;
-text-align: center;
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 11px;
+  line-height: 13px;
+  color: #0f1f2f;
+  text-align: center;
+
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: none;
+  }
 `;
 
 const Tagline = props => {
   return (
     <>
-    <Container>
-      <Headline>{props.headline}</Headline>
-      <Slogan>{props.slogan}</Slogan>
+      <Container>
+        <Headline>{props.headline}</Headline>
+        <Slogan>{props.slogan}</Slogan>
       </Container>
     </>
   );
