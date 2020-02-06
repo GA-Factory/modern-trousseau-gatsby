0d9713003b9c5a0dcec7838c45266a5fca82e944
Roger Campbell II <rc1336@gmail.com>
2020-02-06 11:20:21 -0500

    Started building REST API call. Next Step: Finsih setting up API call and sucessfully pull in data.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,25 @@
  */
 
 // You can delete this file if you're not using it
+const axios = require('axios')
+const path = "https://cdn.contentful.com/spaces/2n4rer1mpqy8/entries/"
+
+
+axios.get(path, {
+    params: {
+        access_token: "zmHoMF9Kow4AAaDPjwNMu5-BNtdMiVJ_yrC9K1RHFyg",
+        select: "fields.name, fields.category",
+        content_type: "Gowns"
+    }
+})
+    .then(function (res) {
+        console.log(res);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
+
+https://cdn.contentful.com/spaces/2n4rer1mpqy8/entries/?access_token=zmHoMF9Kow4AAaDPjwNMu5-BNtdMiVJ_yrC9K1RHFyg
+
+& select=fields.name & content_type=Gown
