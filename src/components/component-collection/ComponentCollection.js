66d432f9a045b4517dd007473e326f211c9a134e
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-06 15:34:43 -0500

    ready for review

--- src/components/component-collection/ComponentCollection.js
+++ src/components/component-collection/ComponentCollection.js
@@ -1,58 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./ComponentCollection.css";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
-// import variables from '../../styles/styled-components'
-
 
 const Container = styled.div`
-    width: 100%;
-    height: 180px;
-    background-color: ${props => props.theme.colors.primaryPink};
-    @media (min-width: ${props => props.theme.breakpoints.laptop}){
-        background-color: red;
-        height: 300px
-    }
-`
+  width: 100%;
+  height: 200px;
+  padding: 20px 20px 20px 40px;
+  background-color: ${props => props.theme.colors.primaryPink};
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    height: 280px;
+    padding: 30px 10px 10px 10px;
+  }
+`;
 
 const GownImageMobile = styled.img`
-    width: 120px;
-    height: 140px;
-    margin: 5px;
-    @media (min-width: ${props => props.theme.breakpoints.laptop}){
-        display: inline-block;
-        width: 240px
-    }
-`
+  width: 160px;
+  height: 150px;
+  margin: 0 5px 5px 5px;
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: inline-block;
+    margin: 8px 15px 12px 15px;
+    width: 160px;
+    height: 180px;
+  }
+`;
 
 const GownImageScreen = styled.img`
-    width: 120px;
-    height: 140px;
-    margin: 5px;
-    display: none;
-    @media (min-width: ${props => props.theme.breakpoints.laptop}){
-        display: inline-block;
-    }
-`
+  width: 120px;
+  height: 150px;
+  margin: 0 5px 5px 5px;
+  display: none;
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: inline-block;
+    margin: 8px 15px 12px 15px;
+    width: 160px;
+    height: 180px;
+  }
+`;
 
-const Title = styled.p `
-    text-align: center;
-    font-weight: bold;
-    font-family: ${props => props.theme.fontStyles.h1Styles.fontStyle}
-`
+const Title = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-family: ${props => props.theme.fontStyles.h1Styles.fontStyle};
+`;
 
-const ComponentCollection = ({images}) => (
-    <>
-        <Container>
-            <GownImageMobile src={images[0]} className='image first'></GownImageMobile>
-            <GownImageMobile src={images[1]} className='image second'></GownImageMobile>
-            <GownImageScreen src={images[2]} className='image third'></GownImageScreen>
-            <GownImageScreen src={images[3]} className='image fourth'></GownImageScreen>
-            <Title>Fall Collection 2020</Title>
-        </Container>
-    </>
-)
+const ComponentCollection = ({ images }) => (
+  <>
+    <Container>
+      <GownImageMobile
+        src={images[0]}
+        className="image first"
+      ></GownImageMobile>
+      <GownImageMobile
+        src={images[1]}
+        className="image second"
+      ></GownImageMobile>
+      <GownImageScreen
+        src={images[2]}
+        className="image third"
+      ></GownImageScreen>
+      <GownImageScreen
+        src={images[3]}
+        className="image fourth"
+      ></GownImageScreen>
+      <Title>Fall Collection 2020</Title>
+    </Container>
+  </>
+);
 
-
-export default ComponentCollection
+export default ComponentCollection;
