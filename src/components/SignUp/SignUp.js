7643f534eb15f015ebd41df5bc275dc901f68a8a
robertsbaer <robertsbaer@yahoo.com>
2020-02-06 15:54:46 -0500

    event handler for submit

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,20 +4,36 @@ import Button from '../Button/Button'
 import './SignUp.css'
 
 
-const SignUp = () => {
-    return (
+class SignUp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('Email was submited: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+          <div className="wrapper">
+              <h5 className="title">Obsessed with a dress?</h5>
+        <form onSubmit={this.handleSubmit}>
+            <input className="textInput" type="text" placeholder="sign up for updates!" value={this.state.value} onChange={this.handleChange} />
+            <Button type="submit" type="light-pink" label="Submit" value="Submit" />
+        </form>
 
-        <>
-            <div className="wrapper">
-            <h5 className="title">Obsessed with a dress?</h5><br></br>
-            <div className="text-button">
-            <input className="textInput" placeholder="sign up for updates!"/>
-            <Button className="button-size"label="Submit" type="light-pink" />
-            </div>
-            </div>
-        </>
-
-    )
-}
+        </div>
+      );
+    }
+  }
 
 export default SignUp
