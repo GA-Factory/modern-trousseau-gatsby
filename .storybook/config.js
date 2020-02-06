7dea65d0b410199a38ab12423aaf91fb9004b347
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-06 11:47:58 -0500

    commit to switch branches and check why themes are not working on component-collection branch

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import styled { ThemeProvider }from 'styled-components';
+import styled from 'styled-components';
 import GlobalStyle from '../src/global/GlobalStyle';
-import variables from '../src/styles/styled-components'
-import { ThemeProvider } from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
@@ -15,12 +13,10 @@ const Wrapper = styled.div`
 `;
 
 const Decorator = storyFn => (
-  <ThemeProvider theme={variables}>
-      <Wrapper>
-        <GlobalStyle />
-        {storyFn()}
-      </Wrapper>
-  </ThemeProvider>
+  <Wrapper>
+    <GlobalStyle />
+    {storyFn()}
+  </Wrapper>
 );
 
 addDecorator(Decorator);
