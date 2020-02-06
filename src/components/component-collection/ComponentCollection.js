df1e19d342641aef6f4f33913dc5b877adafc56a
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-06 16:15:05 -0500

    edits done

--- src/components/component-collection/ComponentCollection.js
+++ src/components/component-collection/ComponentCollection.js
@@ -3,65 +3,95 @@ import ReactDOM from "react-dom";
 import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
 
+const Collection = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+background-color: ${props => props.theme.colors.primaryPink};
+@media (min-width: ${props => props.theme.breakpoints.tablet}) {
+  height: 360px;
+  padding: 30px 10px 10px 10px;
+}
+`;
+
 const Container = styled.div`
+  display: flex;
   width: 100%;
   height: 200px;
   padding: 20px 20px 20px 40px;
+  justify-content: space-around
   background-color: ${props => props.theme.colors.primaryPink};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     height: 280px;
-    padding: 30px 10px 10px 10px;
+    padding: 10px 10px 10px 10px;
   }
 `;
 
-const GownImageMobile = styled.img`
-  width: 160px;
-  height: 150px;
-  margin: 0 5px 5px 5px;
+const GownImageMobile1 = styled.img`
+  width: 148px;
+  height: 157px;
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     display: inline-block;
-    margin: 8px 15px 12px 15px;
-    width: 160px;
-    height: 180px;
+    width: 260px;
+    height: 267px;
   }
 `;
 
-const GownImageScreen = styled.img`
+const GownImageMobile2 = styled.img`
+  width: 148px;
+  height: 157px;
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: inline-block;
+    width: 250px;
+    height: 267px;
+  }
+`;
+
+const GownImageScreen1 = styled.img`
   width: 120px;
   height: 150px;
-  margin: 0 5px 5px 5px;
   display: none;
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     display: inline-block;
-    margin: 8px 15px 12px 15px;
-    width: 160px;
-    height: 180px;
+    width: 200px;
+    height: 267px;
+  }
+`;
+
+const GownImageScreen2 = styled.img`
+  width: 120px;
+  height: 150px;
+  display: none;
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: inline-block;
+    width: 260px;
+    height: 267px;
   }
 `;
 
 const Title = styled.p`
   text-align: center;
   font-weight: bold;
+  margin-bottom: 8px;
   font-family: ${props => props.theme.fontStyles.h1Styles.fontStyle};
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    font-family: ${props => props.theme.fontStyles.h3Styles.fontStyle};
+    font-size: ${props => props.theme.fontStyles.h3Styles.fontSize};
+    margin-top: 10px
+  }
 `;
 
 const ComponentCollection = ({ images }) => (
   <>
-    <Container>
-      <GownImageMobile
-        src={images[0]}
-      ></GownImageMobile>
-      <GownImageMobile
-        src={images[1]}
-      ></GownImageMobile>
-      <GownImageScreen
-        src={images[2]}
-      ></GownImageScreen>
-      <GownImageScreen
-        src={images[3]}
-      ></GownImageScreen>
+    <Collection>
+      <Container>
+        <GownImageScreen1 src={images[0]}></GownImageScreen1>
+        <GownImageMobile1 src={images[1]}></GownImageMobile1>
+        <GownImageScreen2 src={images[2]}></GownImageScreen2>
+        <GownImageMobile2 src={images[3]}></GownImageMobile2>
+      </Container>
       <Title>Fall Collection 2020</Title>
-    </Container>
+    </Collection>
   </>
 );
 
--- src/components/component-collection/ComponentCollection.stories.js
+++ src/components/component-collection/ComponentCollection.stories.js
@@ -3,10 +3,10 @@ import { storiesOf } from "@storybook/react";
 
 import ComponentCollection from "./ComponentCollection";
 
-import imageFile1 from "./images/test-image-1.png";
-import imageFile2 from "./images/test-image-2.png";
-import imageFile3 from "./images/test-image-3.png";
-import imageFile4 from "./images/test-image-4.png";
+import imageFile1 from "../../assets/images/fall-collection-1.png";
+import imageFile2 from "../../assets/images/fall-collection-2.png";
+import imageFile3 from "../../assets/images/fall-collection-3.png";
+import imageFile4 from "../../assets/images/fall-collection-4.png";
 
 const images = [imageFile1, imageFile2, imageFile3, imageFile4];
 
