b8d580fc7cd1f4e042c719bd647e1f63a2b7c689
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-06 10:41:46 -0500

    working on getting dimension specs and @media accurate to mockup

--- src/components/component-collection/ComponentCollection.js
+++ src/components/component-collection/ComponentCollection.js
@@ -2,13 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./ComponentCollection.css";
 
-const ComponentCollection = ({images}) => (
+import imageFile1 from "./images/test-image-1.png";
+import imageFile2 from "./images/test-image-2.png";
+import imageFile3 from "./images/test-image-3.png";
+import imageFile4 from "./images/test-image-4.png";
+
+const images = [
+    imageFile1, 
+    imageFile2, 
+    imageFile3, 
+    imageFile4
+]
+
+const ComponentCollection = () => (
     <>
         <div className='component-collection'>
-            <img src='images[0]' className='image'></img>
-            <img src='images[0]' className='image'></img>
-            <img src='images[0]' className='image'></img>
-            <img src='images[0]' className='image'></img>
+            <img src='images[0]' className='image first'></img>
+            <img src='images[0]' className='image second'></img>
+            <img src='images[0]' className='image third'></img>
+            <img src='images[0]' className='image fourth'></img>
             <p className='collection-title'>Fall Collection 2020</p>
         </div>
     </>
--- src/components/component-collection/ComponentCollection.stories.js
+++ src/components/component-collection/ComponentCollection.stories.js
@@ -9,7 +9,10 @@ import imageFile3 from "./images/test-image-3.png";
 import imageFile4 from "./images/test-image-4.png";
 
 const images = [
-imageFile1, imageFile2, imageFile3, imageFile4,
+  imageFile1, 
+  imageFile2, 
+  imageFile3, 
+  imageFile4
 ]
 
 storiesOf("Component-Collection", module).add("Component-Collection", () => (
