90e3e15e7ef8aca103a890377f2a4d99ef1084c3
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-06 10:55:22 -0500

    pulling images from .stories, still working on layout and @media

--- src/components/component-collection/ComponentCollection.js
+++ src/components/component-collection/ComponentCollection.js
@@ -2,25 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./ComponentCollection.css";
 
-import imageFile1 from "./images/test-image-1.png";
-import imageFile2 from "./images/test-image-2.png";
-import imageFile3 from "./images/test-image-3.png";
-import imageFile4 from "./images/test-image-4.png";
-
-const images = [
-    imageFile1, 
-    imageFile2, 
-    imageFile3, 
-    imageFile4
-]
-
-const ComponentCollection = () => (
+const ComponentCollection = ({images}) => (
     <>
         <div className='component-collection'>
-            <img src='images[0]' className='image first'></img>
-            <img src='images[0]' className='image second'></img>
-            <img src='images[0]' className='image third'></img>
-            <img src='images[0]' className='image fourth'></img>
+            <img src={images[0]} className='image first'></img>
+            <img src={images[1]} className='image second'></img>
+            <img src={images[2]} className='image third'></img>
+            <img src={images[3]} className='image fourth'></img>
             <p className='collection-title'>Fall Collection 2020</p>
         </div>
     </>
