623af6878c0ebe12b7a548102ba4e6498196d9f0
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-06 14:05:38 -0500

    commit before afternoon lesson.  media query acting funny, afte rthat gets figured out strightforward path to finish this component quickly

--- src/components/component-collection/ComponentCollection.js
+++ src/components/component-collection/ComponentCollection.js
@@ -1,16 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./ComponentCollection.css";
+import styled from 'styled-components';
+import { ThemeProvider } from "styled-components";
+// import variables from '../../styles/styled-components'
+
+
+const Container = styled.div`
+    width: 100%;
+    height: 180px;
+    background-color: ${props => props.theme.colors.primaryPink}
+`
+
+const GownImageMobile = styled.img`
+    width: 120px;
+    height: 140px;
+    margin: 5px;
+`
+
+const GownImageScreen = styled.img`
+    width: 120px;
+    height: 140px;
+    margin: 5px;
+    display: none;
+    @media (min-width: ${props => props.theme.breakpoints.laptop}){
+        display: inline-block;
+    }
+`
+
+const Title = styled.p `
+    text-align: center;
+    font-weight: bold;
+    font-family: ${props => props.theme.fontStyles.h1Styles.fontStyle}
+`
+/* const image = styled. `
+         width: 150px;
+         height: 200px;
+         margin: 20px;
+     ` */
+
+/* @media only screen and (min-width: 480px) 
+    component-collection 
+        width: 100%;
+        height: 260px;
+    ` */
 
 const ComponentCollection = ({images}) => (
     <>
-        <div className='component-collection'>
-            <img src={images[0]} className='image first'></img>
-            <img src={images[1]} className='image second'></img>
-            <img src={images[2]} className='image third'></img>
-            <img src={images[3]} className='image fourth'></img>
-            <p className='collection-title'>Fall Collection 2020</p>
-        </div>
+        <Container>
+            <GownImageMobile src={images[0]} className='image first'></GownImageMobile>
+            <GownImageMobile src={images[1]} className='image second'></GownImageMobile>
+            <GownImageScreen src={images[2]} className='image third'></GownImageScreen>
+            <GownImageScreen src={images[3]} className='image fourth'></GownImageScreen>
+            <Title>Fall Collection 2020</Title>
+        </Container>
     </>
 )
 
