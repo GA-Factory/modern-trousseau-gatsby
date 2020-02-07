fc5b23d25973f8ad11e427070e2083b018f272b3
robertsbaer <robertsbaer@yahoo.com>
2020-02-07 17:30:27 -0500

    logo and mobile ready

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import NavMenu from '../NavMenu/NavMenu'
+import Logo from '../FullLogo/Logo'
+import './Header.css'
+
+
+
+const Header = () => {
+    return (
+        <>
+        <div className="header">
+        <div className="MobNav">
+        <Logo notag/>
+        </div>
+        <div className="Nav">
+        <NavMenu />
+        </div>
+        </div>
+        </>
+  )
+  }
+  
+  export default Header
