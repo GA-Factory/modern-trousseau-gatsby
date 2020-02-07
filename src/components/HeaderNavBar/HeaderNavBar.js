39c648d144bf44906899ef9eb4f404fac6ca7323
Kelly M Collins <90kcol@gmail.com>
2020-02-07 12:23:35 -0500

    working styling of list

--- src/components/HeaderNavBar/HeaderNavBar.js
+++ src/components/HeaderNavBar/HeaderNavBar.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+import './HeaderNavBar.css';
+
 
 const HeaderNavBar = () => (
     <Router>
         <div>
-            <nav>
+            <nav className="header-nav">
                 <ul>
                     <li>
                         <p>OUR GOWNS</p>
+                        <span className="dots">&#9679;</span>
                     </li>
                     <li>
                         <p>VISIT US</p>
+                        <span className="dots">&#9679;</span>
                     </li>
                     <li>
                         <p>BOOK AN APPOINTMENT</p>
+                        <span className="dots">&#9679;</span>
                     </li>
                     <li>
                         <p>ABOUT US</p>
