2886753e0bcd3d1102c1182d14d93378e0d3d32d
Kelly M Collins <90kcol@gmail.com>
2020-02-07 11:04:49 -0500

    completed Nav Bar and made responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,31 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Icons from '../Icons/FAIcon';
-import { BrowserRouter, Link } from "react-router-dom";
 import './NavBar.css';
 import logo from './Logo/Logo.png';
 
 const NavBar = () => (
-    <div>
-        <nav className="nav-bar">
-            <div className="mobile-style">
-                {/* Replace with burger component when complete */}
-                <Icons name={['fas', 'bars']} />
-            </div>
-            <div className="mobile-style">
-                {/* Replace with logo component */}
-                <img src={logo} height="10" width="100" alt="Modern trousseau logo" />
-            </div>
-            <div className="nav-icons">
-                <Icons name={['fas', 'search']} />
-            </div>
-            <div className="desktop-style">
-                <Icons name={['fas', 'map-marker-alt']} />
-            </div>
-            <div className="desktop-style">
-                <Icons name={['far', 'user']} />
-            </div>
-        </nav>
-    </div>
+    <Router>
+        <div>
+            <nav className="nav-bar">
+                <div className="mobile-style">
+                    {/* Replace with burger component when complete */}
+                    <Link to="#">
+                        <Icons name={['fas', 'bars']} size="2x" />
+                    </Link>
+                </div>
+                <div className="mobile-style">
+                    {/* Replace with logo component */}
+                    <img className="mobile-logo" src={logo} alt="Modern trousseau logo" />
+                </div>
+                <div className="mobile-style">
+                    <Link to="#">
+                        <Icons name={['fas', 'search']} size="2x" />
+                    </Link>
+                </div>
+                <div className="desktop-style">
+                    <Link to="#">
+                        <Icons name={['fas', 'search']} />
+                    </Link>
+                </div>
+                <div className="desktop-style">
+                    <Link to="#">
+                        <Icons name={['fas', 'map-marker-alt']} />
+                    </Link>
+                </div>
+                <div className="desktop-style">
+                    <Link to="#">
+                        <Icons name={['far', 'user']} />
+                    </Link>
+                </div>
+            </nav>
+        </div>
+    </Router>
 );
 
 export default NavBar;
