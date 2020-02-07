55f58f909aaaf13a4263ca5be0c2ada38d6393a3
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-06 23:54:53 -0500

    css is almost there for layout, still need to work on text and button placement

--- src/components/BrideReview/BrideReview.js
+++ src/components/BrideReview/BrideReview.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
-import Button from '../Button/Button'
+import Button from "../Button/Button";
 
 const BridalReviews = styled.div`
-width: 100%;
-background-color: ${props => props.theme.colors.backgroundGray};
-@media (min-width: ${props => props.theme.breakpoints.tablet}) {
-  height: 300px;
-}
+  width: 100%;
+  background-color: ${props => props.theme.colors.backgroundGray};
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    height: 300px;
+  }
 `;
 
 const Container = styled.div`
@@ -18,48 +18,54 @@ const Container = styled.div`
   justify-content: space-around;
   background-color: ${props => props.theme.colors.backgroundGray};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    height: 400px;
+    height: 327px;
   }
 `;
 
 const ReviewContainer1 = styled.div`
   display: flex;
+  /* justify-content: space-around; */
   height: 208.55px;
   width: 296px;
   border: 1px solid black;
   justify-content: space-around;
+  margin: 15px 5px 5px 5px;
   background-color: ${props => props.theme.colors.backgroundGray};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    height: 240px;
-    width: 405px;
+      height: 240px;
+      width: 405px;
+      justify-content: space-around;
+      margin-top: 40px
   }
 `;
 
 const ReviewContainer2 = styled.div`
-  /* display: flex; */
   height: 200px;
   justify-content: space-around;
   display: none;
   background-color: ${props => props.theme.colors.backgroundGray};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    display: flex;
-    border: 1px solid black;
-    height: 240px;
-    width: 405px;
+      display: flex;
+      border: 1px solid black;
+      height: 240px;
+      width: 405px;
+      justify-content: space-around;
+      margin-top: 40px
   }
 `;
 
 const ReviewContainer3 = styled.div`
-  /* display: flex; */
   height: 200px;
   justify-content: space-around;
   display: none;
   background-color: ${props => props.theme.colors.backgroundGray};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    display: flex;
-    border: 1px solid black;
-    height: 240px;
-    width: 405px;
+      display: flex;
+      border: 1px solid black;
+      height: 240px;
+      width: 405px;
+      justify-content: space-around;
+      margin-top: 40px
   }
 `;
 
@@ -132,81 +138,103 @@ const ImageAccompaniment3 = styled.div`
   }
 `;
 
-// const ImageAccompaniment2 = styled.div`
-//   height: 208.55px;
-//   width: 296px;
-//   border: 1px solid black;
-//   justify-content: space-around;
-//   background-color: ${props => props.theme.colors.backgroundGray};
-//   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-//     height: 240px;
-//     width: 405px;
-//   }
-// `;
-
-// const ImageAccompaniment3 = styled.div`
-//   height: 208.55px;
-//   width: 296px;
-//   border: 1px solid black;
-//   justify-content: space-around;
-//   background-color: ${props => props.theme.colors.backgroundGray};
-//   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-//     height: 240px;
-//     width: 405px;
-//   }
-// `;
-
-const Review1 = styled.div`
-    font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
-    font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
-    text-align: center;
+const MobileReview1 = styled.div`
+  font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
+  font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
+  text-align: center;
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: none;
+  }
 `;
 
-const Review2 = styled.div`
-    font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
-    font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
-    text-align: center;
+
+const ScreenReview1 = styled.div`
+  font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
+  font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
+  text-align: center;
+  display: none;
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: inline-block;
+    height: 240px;
+    width: 197px;
+  }
 `;
 
-const Review3 = styled.div`
-    font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
-    font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
-    text-align: center;
-`
+const ScreenReview2 = styled.div`
+  font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
+  font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
+  text-align: center;
+`;
+
+const ScreenReview3 = styled.div`
+  font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
+  font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
+  text-align: center;
+`;
 
 const ComponentCollection = ({ images }) => (
   <>
     <BridalReviews>
       <Container>
         <ReviewContainer1>
-            <GownImageMobile1 src={images[0]}></GownImageMobile1>
-            <ImageAccompaniment1>
-                <Review1>From start to finish, my dress was high quality, hassle-free, and ready when promised.” - Modern Trousseau Bride</Review1>
-                <button style={{
-                    border: '1px solid black'
-                }}>View Dress</button>
-                {/* <Button></Button> */}
-            </ImageAccompaniment1>
+          <GownImageMobile1 src={images[0]}></GownImageMobile1>
+          <ImageAccompaniment1>
+            <MobileReview1>
+              From start to finish, my dress was high quality, hassle-free, and
+              ready when promised.” - Modern Trousseau Bride
+            </MobileReview1>
+            <ScreenReview1>They made me feel like a queen and I was extremely impressed with the quality of their gowns. Plus, I loved that they actually take your measurements and make it YOUR size.</ScreenReview1>
+            <button
+              style={{
+                border: "1px solid black",
+                display: "block",
+                margin: "auto"
+              }}
+            >
+              View Dress
+            </button>
+            {/* <Button></Button> */}
+          </ImageAccompaniment1>
         </ReviewContainer1>
         <ReviewContainer2>
-            <GownImageScreen1 src={images[1]}></GownImageScreen1>
-            <ImageAccompaniment2>
-                <Review2>From start to finish, my dress was high quality, hassle-free, and ready when promised.” - Modern Trousseau Bride</Review2>
-                <button style={{
-                    border: '1px solid black'
-                }}>View Dress</button>
-                {/* <Button></Button> */}
-            </ImageAccompaniment2>
+          <GownImageScreen1 src={images[1]}></GownImageScreen1>
+          <ImageAccompaniment2>
+            <ScreenReview2>
+              From start to finish, my dress was high quality, hassle-free, and
+              ready when promised.” - Modern Trousseau Bride
+            </ScreenReview2>
+            <button
+              style={{
+                border: "1px solid black",
+                display: "block",
+                margin: "auto",
+                // position: "absolute",
+                // right:    "0",
+                // bottom:   "0"
+              }}
+            >
+              View Dress
+            </button>
+            {/* <Button></Button> */}
+          </ImageAccompaniment2>
         </ReviewContainer2>
         <ReviewContainer3>
-            <GownImageScreen2 src={images[2]}></GownImageScreen2>
-            <ImageAccompaniment3>
-                <Review3>From start to finish, my dress was high quality, hassle-free, and ready when promised.” - Modern Trousseau Bride</Review3>
-                <button style={{
-                    border: '1px solid black'
-                }}>View Dress</button>
-                {/* <Button></Button> */}
-            </ImageAccompaniment3>
+          <GownImageScreen2 src={images[2]}></GownImageScreen2>
+          <ImageAccompaniment3>
+            <ScreenReview3>
+            Everything about the dress was perfect - the quality, fit, and overall look was impeccable. Thank you for your amazing customer service and beautiful product.
+            </ScreenReview3>
+            <button
+              style={{
+                border: "1px solid black",
+                display: "block",
+                margin: "auto"
+              }}
+            >
+              View Dress
+            </button>
+            {/* <Button></Button> */}
+          </ImageAccompaniment3>
         </ReviewContainer3>
       </Container>
     </BridalReviews>
@@ -214,7 +242,3 @@ const ComponentCollection = ({ images }) => (
 );
 
 export default ComponentCollection;
-
-            
-            
-
