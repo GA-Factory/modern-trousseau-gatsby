595c7557ec35b63ca32549114b59ffc717d3b9e7
Adam_Bates <Creizai@EMAIL.COM>
2020-02-07 11:32:04 -0500

    removed card.css transfered that code into component styling in the card js folder and then used the color variables that have been supplied

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import './Card.css'
-import Icons from '../Icons/FAIcon'
+import React from "react";
+import "./Card.css";
+import Icons from "../Icons/FAIcon";
+import styled from "styled-components";
 
-const Card = (props) => {
-	return(
-		<div className='card-container primary-pink'>
-			<Icons name={props.icon} size='3x' />
-			<div className='card-title'>{props.title}</div>
-			<p className='card-paragraph'>{props.paragraph}</p>
-		</div>
-	)
-}
+const CardContainer = styled.div`
+  // was styled with className primary-pink
+  background-color: ${props => props.theme.colors.primaryPink};
+  max-height: 169px;
+  max-width: 234px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  padding: 14px 24px;
+`;
 
-export default Card
+const CardTitle = styled.div`
+  margin: 10px 0 0 0;
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 15px;
+  text-align: center;
+`;
+
+const CardParagraph = styled.p`
+  margin: 0;
+  text-align: center;
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 13px;
+  line-height: 15px;
+  text-align: center;
+`;
+
+const Card = props => {
+  return (
+    <CardContainer>
+      <Icons name={props.icon} size="3x" />
+      <CardTitle>{props.title}</CardTitle>
+      <CardParagraph>{props.paragraph}</CardParagraph>
+    </CardContainer>
+  );
+};
+
+export default Card;
