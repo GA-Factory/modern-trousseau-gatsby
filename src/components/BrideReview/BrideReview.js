7f035b111e5b135635b6c993a48685241be846f5
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-06 23:20:26 -0500

    all elements in place except for ViewDress button and mobile slider button...need link for view dress button also

--- src/components/BrideReview/BrideReview.js
+++ src/components/BrideReview/BrideReview.js
@@ -36,12 +36,13 @@ const ReviewContainer1 = styled.div`
 `;
 
 const ReviewContainer2 = styled.div`
+  /* display: flex; */
   height: 200px;
   justify-content: space-around;
   display: none;
   background-color: ${props => props.theme.colors.backgroundGray};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    display: inline-block;
+    display: flex;
     border: 1px solid black;
     height: 240px;
     width: 405px;
@@ -49,12 +50,13 @@ const ReviewContainer2 = styled.div`
 `;
 
 const ReviewContainer3 = styled.div`
+  /* display: flex; */
   height: 200px;
   justify-content: space-around;
   display: none;
   background-color: ${props => props.theme.colors.backgroundGray};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    display: inline-block;
+    display: flex;
     border: 1px solid black;
     height: 240px;
     width: 405px;
@@ -94,7 +96,7 @@ const GownImageScreen2 = styled.img`
   }
 `;
 
-const ImageAccompaniment = styled.div`
+const ImageAccompaniment1 = styled.div`
   height: 208.55px;
   width: 296px;
   border: 1px solid black;
@@ -106,9 +108,70 @@ const ImageAccompaniment = styled.div`
   }
 `;
 
+const ImageAccompaniment2 = styled.div`
+  height: 208.55px;
+  width: 296px;
+  border: 1px solid black;
+  justify-content: space-around;
+  background-color: ${props => props.theme.colors.backgroundGray};
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    height: 240px;
+    width: 405px;
+  }
+`;
+
+const ImageAccompaniment3 = styled.div`
+  height: 208.55px;
+  width: 296px;
+  border: 1px solid black;
+  justify-content: space-around;
+  background-color: ${props => props.theme.colors.backgroundGray};
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    height: 240px;
+    width: 405px;
+  }
+`;
+
+// const ImageAccompaniment2 = styled.div`
+//   height: 208.55px;
+//   width: 296px;
+//   border: 1px solid black;
+//   justify-content: space-around;
+//   background-color: ${props => props.theme.colors.backgroundGray};
+//   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+//     height: 240px;
+//     width: 405px;
+//   }
+// `;
+
+// const ImageAccompaniment3 = styled.div`
+//   height: 208.55px;
+//   width: 296px;
+//   border: 1px solid black;
+//   justify-content: space-around;
+//   background-color: ${props => props.theme.colors.backgroundGray};
+//   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+//     height: 240px;
+//     width: 405px;
+//   }
+// `;
+
 const Review1 = styled.div`
+    font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
+    font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
+    text-align: center;
+`;
 
+const Review2 = styled.div`
+    font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
+    font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
+    text-align: center;
+`;
 
+const Review3 = styled.div`
+    font-style: ${props => props.theme.fontStyles.Body3DesktopText.fontStyle};
+    font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
+    text-align: center;
 `
 
 const ComponentCollection = ({ images }) => (
@@ -117,19 +180,33 @@ const ComponentCollection = ({ images }) => (
       <Container>
         <ReviewContainer1>
             <GownImageMobile1 src={images[0]}></GownImageMobile1>
-            <ImageAccompaniment>
-            <Review1>From start to finish, my dress was high quality, hassle-free, and ready when promised.” - Modern Trousseau Bride</Review1>
-            <button style={{
-                border: '1px solid black'
-            }}>View Dress</button>
-            {/* <Button></Button> */}
-            </ImageAccompaniment>
+            <ImageAccompaniment1>
+                <Review1>From start to finish, my dress was high quality, hassle-free, and ready when promised.” - Modern Trousseau Bride</Review1>
+                <button style={{
+                    border: '1px solid black'
+                }}>View Dress</button>
+                {/* <Button></Button> */}
+            </ImageAccompaniment1>
         </ReviewContainer1>
         <ReviewContainer2>
             <GownImageScreen1 src={images[1]}></GownImageScreen1>
+            <ImageAccompaniment2>
+                <Review2>From start to finish, my dress was high quality, hassle-free, and ready when promised.” - Modern Trousseau Bride</Review2>
+                <button style={{
+                    border: '1px solid black'
+                }}>View Dress</button>
+                {/* <Button></Button> */}
+            </ImageAccompaniment2>
         </ReviewContainer2>
         <ReviewContainer3>
             <GownImageScreen2 src={images[2]}></GownImageScreen2>
+            <ImageAccompaniment3>
+                <Review3>From start to finish, my dress was high quality, hassle-free, and ready when promised.” - Modern Trousseau Bride</Review3>
+                <button style={{
+                    border: '1px solid black'
+                }}>View Dress</button>
+                {/* <Button></Button> */}
+            </ImageAccompaniment3>
         </ReviewContainer3>
       </Container>
     </BridalReviews>
