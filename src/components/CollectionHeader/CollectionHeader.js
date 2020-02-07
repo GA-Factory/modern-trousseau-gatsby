3b1dd575ae34c993e58ee027dc6d72a25324f3b0
Mamady Sillah <supurpisces310@msn.com>
2020-02-07 12:52:19 -0500

    added h5 header and styled it. Also imported icon; next step is to align arrow to left of the header

--- src/components/CollectionHeader/CollectionHeader.js
+++ src/components/CollectionHeader/CollectionHeader.js
@@ -1,22 +1,31 @@
 
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../CollectionHeader/CollectionHeader.css"
 
+library.add(faArrowLeft)
+let icon = (faArrowLeft)
 
 
-const CollectionHeader = () => {
-    return (
-        <>
-            <div class="con">
-            <h5>Stay Connected</h5><br />
-            <div class="some">
-            <img  id="fb" src={facebook}></img>
-            <img  id="in" src={instagram}></img>
-            <img  id="pi" src={pinterest}></img>
-            </div>
-            </div>
-        </>
-    )
+class CollectionHeader extends React.Component {
+    
+   render() {
+       
+      return (
+         <div className="collect-header">
+          <a className="collection-arrow">
+              <FontAwesomeIcon icon={icon}/>
+          </a>
+          <h5 className="collection-header">Spring 2020 Collection</h5>
+               
+          </div>
+      )
+   }
 }
 
-export default CollectionHeader
+export default CollectionHeader;
+
+// {/* <FontAwesomeIcon icon = {['fas', 'arrow-left']} size = 'xs'/> */}
+
