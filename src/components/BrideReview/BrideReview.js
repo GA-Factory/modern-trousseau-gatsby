e1de02faa639181ecdbb4a40b3e21a94925454ac
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-07 10:42:01 -0500

    ready for review

--- src/components/BrideReview/BrideReview.js
+++ src/components/BrideReview/BrideReview.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
 import Button from "../Button/Button";
+import PinkButton from "../Button/Button";
 
 const BridalReviews = styled.div`
   width: 100%;
@@ -28,7 +29,7 @@ const ReviewContainer1 = styled.div`
   width: 296px;
   justify-content: space-around;
   margin: 15px 5px 5px 5px;
-  padding-left:5px;
+  padding-left: 5px;
   background-color: ${props => props.theme.colors.backgroundGray};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     height: 240px;
@@ -147,7 +148,6 @@ const ScreenReview1 = styled.div`
   display: none;
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     display: inline-block;
-    margin-left: 16px;
     position: absolute;
     top: 35%;
     width: 189px;
@@ -160,7 +160,7 @@ const ScreenReview2 = styled.div`
   text-align: center;
   position: absolute;
   top: 42%;
-  margin-left: 6px;
+  margin-left: 2px;
   width: 193px;
 `;
 
@@ -169,9 +169,18 @@ const ScreenReview3 = styled.div`
   font-size: ${props => props.theme.fontStyles.Body3DesktopText.fontSize};
   text-align: center;
   position: absolute;
-  top: 36%;
-  margin-left: 6px;
+  top: 35%;
+  margin-left: 2px;
   width: 175px;
+`;
+
+const ViewDress = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    margin-top: 200px;
+  }
 `;
 
 const MobileReviewSliderButton = styled.button`
@@ -179,7 +188,7 @@ const MobileReviewSliderButton = styled.button`
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     display: none;
   }
-`
+`;
 
 const ComponentCollection = ({ images }) => (
   <>
@@ -192,61 +201,88 @@ const ComponentCollection = ({ images }) => (
               "From start to finish, my dress was high quality, hassle-free, and
               ready when promised.”<br></br> - Modern Trousseau Bride
             </MobileReview1>
-            <ScreenReview1>
-              "They made me feel like a queen and I was extremely impressed with
-              the quality of their gowns. Plus, I loved that they actually take
-              your measurements and make it YOUR size."
-            </ScreenReview1>
-            <button
-              style={{
-                border: "1px solid black",
-                display: "block",
-                margin: "auto",
-              }}
-            >
-              View Dress
-            </button>
-            <Button />
+            <ViewDress>
+              <ScreenReview1>
+                "They made me feel like a queen and I was extremely impressed
+                with the quality of their gowns. Plus, I loved that they
+                actually take your measurements and make it YOUR size."
+              </ScreenReview1>
+              <PinkButton
+                label="View Dress"
+                fontsize={`${"14px"}`}
+                weight={`${"bold"}`}
+                height={`${"22px"}`}
+                width={`${"88px"}`}
+                padding={`${"5px 0"}`}
+                shadow={`${"0px 4px 4px rgba(0, 0, 0, 0.25)"}`}
+                desktopwidth={`${"83px"}`}
+                desktoppadding={`${"5px"}`}
+                desktopheight={`${"14px"}`}
+                desktopfontsize={`${"12px"}`}
+                desktopweight={`${"600"}`}
+                desktopcolor={`${"#000000"}`}
+                family={`${"'Sahitya'"}`}
+                color={`${"#441F0E"}`}
+                />
+                </ViewDress>
           </ImageAccompaniment1>
-          <MobileReviewSliderButton>
-          &#10095;
-          </MobileReviewSliderButton>
+          <MobileReviewSliderButton>&#10095;</MobileReviewSliderButton>
         </ReviewContainer1>
         <ReviewContainer2>
           <GownImageScreen1 src={images[1]}></GownImageScreen1>
           <ImageAccompaniment2>
-            <ScreenReview2>
-              "From start to finish, my dress was high quality, hassle-free, and
-              ready when promised.”
-            </ScreenReview2>
-            <button
-              style={{
-                border: "1px solid black",
-                display: "block",
-                margin: "auto"
-              }}
-            >
-              View Dress
-            </button>
+            <ViewDress>
+              <ScreenReview2>
+                "From start to finish, my dress was high quality, hassle-free,
+                and ready when promised.”
+              </ScreenReview2>
+              <PinkButton
+                label="View Dress"
+                fontsize={`${"14px"}`}
+                weight={`${"bold"}`}
+                height={`${"22px"}`}
+                width={`${"88px"}`}
+                padding={`${"5px 0"}`}
+                shadow={`${"0px 4px 4px rgba(0, 0, 0, 0.25)"}`}
+                desktopwidth={`${"83px"}`}
+                desktoppadding={`${"5px"}`}
+                desktopheight={`${"14px"}`}
+                desktopfontsize={`${"12px"}`}
+                desktopweight={`${"600"}`}
+                desktopcolor={`${"#000000"}`}
+                family={`${"'Sahitya'"}`}
+                color={`${"#441F0E"}`}
+              />
+            </ViewDress>
           </ImageAccompaniment2>
         </ReviewContainer2>
         <ReviewContainer3>
           <GownImageScreen2 src={images[2]}></GownImageScreen2>
           <ImageAccompaniment3>
-            <ScreenReview3>
-              "Everything about the dress was perfect - the quality, fit, and
-              overall look was impeccable. Thank you for your amazing customer
-              service and beautiful product."
-            </ScreenReview3>
-            <button
-              style={{
-                border: "1px solid black",
-                display: "block",
-                margin: "auto"
-              }}
-            >
-              View Dress
-            </button>
+            <ViewDress>
+              <ScreenReview3>
+                "Everything about the dress was perfect - the quality, fit, and
+                overall look was impeccable. Thank you for your amazing customer
+                service and beautiful product."
+              </ScreenReview3>
+              <PinkButton
+                label="View Dress"
+                fontsize={`${"14px"}`}
+                weight={`${"bold"}`}
+                height={`${"22px"}`}
+                width={`${"88px"}`}
+                padding={`${"5px 0"}`}
+                shadow={`${"0px 4px 4px rgba(0, 0, 0, 0.25)"}`}
+                desktopwidth={`${"83px"}`}
+                desktoppadding={`${"5px"}`}
+                desktopheight={`${"14px"}`}
+                desktopfontsize={`${"12px"}`}
+                desktopweight={`${"600"}`}
+                desktopcolor={`${"#000000"}`}
+                family={`${"'Sahitya'"}`}
+                color={`${"#441F0E"}`}
+              />
+            </ViewDress>
           </ImageAccompaniment3>
         </ReviewContainer3>
       </Container>
