3be76a1f13030ed1841b719ea567fb29f0b36a68
Igor Slabykh <igor.slabykh@gmail.com>
2020-02-10 15:09:51 -0500

    responsive design works. need to delete unused code and check

--- src/components/DressListItem/DressListItem.js
+++ src/components/DressListItem/DressListItem.js
@@ -1,18 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 import "./DressListItem.css";
+import imageFile1 from "../DressListItem/DressListItemAssets/seraphina.png";
 
-const DressListItem = props => (
-  <div>
-    <div className="primary-pink">
-      <div className="dress-list-item-container">
-        <img src={props.image1} alt="image" />
-      </div>
-
-      <h5 className="season-collection-text">
-        {props.season} {props.year} Collection
-      </h5>
+export default () => (
+  <div className="one-item">
+    <div className="dress-list-item-container">
+      <img src={imageFile1} alt="image" />
+    </div>
+    <div>
+      <h5 className="dress-name">{"Seraphina"}</h5>
     </div>
   </div>
 );
 
-export default DressListItem;
+// const DesktopCard = styled.div`
+//   display: none;
+//   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+//     background-color: red;
+//   }
+// `;
+
+// const DressListItem = props => (
+//   <div>
+//     <div className="primary-pink">
+//       <div className="dress-list-item-container">
+//         <img src={props.image1} alt="image" />
+//       </div>
+
+//       <h5 className="dress-name">{props.dressname}</h5>
+//     </div>
+//   </div>
+// );
+
+// export default DressListItem;
+
+// ${props => props.theme.colors.primaryPink}
--- src/components/DressListItem/DressListItem.stories.js
+++ src/components/DressListItem/DressListItem.stories.js
@@ -5,5 +5,5 @@ import imageFile1 from "../DressListItem/DressListItemAssets/seraphina.png";
 import DressListItem from "./DressListItem";
 
 storiesOf("DressListItem", module).add("DressListItem", () => (
-  <DressListItem image1={imageFile1} season={"Fall"} year={"2020"} />
+  <DressListItem image1={imageFile1} dressname={"Seraphina"} />
 ));
