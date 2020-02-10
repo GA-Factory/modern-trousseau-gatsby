d0a3e9aeeada1d4bfce5d10ff97643bcda7c6d4a
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-10 10:30:15 -0500

    waiting on collectionList merge, updated here for now

--- src/pages/GownsPage.js
+++ src/pages/GownsPage.js
@@ -3,62 +3,10 @@ import ReactDOM from "react-dom";
 import styled from "styled-components";
 import Logo from '../components/FullLogo/Logo'
 import Icons from '../components/Icons/FAIcon'
-import CollectionList from '../components/CollectionList/CollectionList'
+// import CollectionList from '../components/CollectionList/CollectionList'
 
-// import { slideData } from '../components/CollectionList/CollectionList.stories'
+// import slideData from '../components/CollectionList/CollectionList.stories'
 import Layout from '../global/Layout';
-
-// import imageFile1 from "../assets/images/Fall-2020-1.png";
-// import imageFile2 from "../assets/images/Fall-2020-2.png";
-// import imageFile3 from "../assets/images/Spring-2020-1.jpeg";
-// import imageFile4 from "../assets/images/Spring-2020-2.jpeg";
-// import imageFile5 from "../assets/images/Trending-Now-1.jpeg";
-// import imageFile6 from "../assets/images/Trending-Now-2.jpeg";
-// import imageFile7 from "../assets/images/Classics-1.jpeg";
-// import imageFile8 from "../assets/images/Classics-2.jpeg";
-
-// const slideData = [
-//   {
-//     image: imageFile1,
-//     label: "LUCA",
-//     index: 0
-//   },
-//   {
-//     image: imageFile2,
-//     label: "VIDA",
-//     index: 1
-//   },
-//   {
-//     image: imageFile3,
-//     label: "ALWAYS",
-//     index: 2
-//   },
-//   {
-//     image: imageFile4,
-//     label: "BELIZE",
-//     index: 3
-//   },
-//   {
-//     image: imageFile5,
-//     label: "ALICE",
-//     index: 4
-//   },
-//   {
-//     image: imageFile6,
-//     label: "EVELYN",
-//     index: 5
-//   },
-//   {
-//     image: imageFile7,
-//     label: "AINSLEY",
-//     index: 6
-//   },
-//   {
-//     image: imageFile8,
-//     label: "ANNIE",
-//     index: 7
-//   }
-// ];
 
 const GownsPage = () => (
     <Layout>
@@ -68,7 +16,7 @@ const GownsPage = () => (
         }}
             />
         <Logo notag ></Logo>
-        {/* <CollectionList ></CollectionList> */}
+        {/* <CollectionList slideData={slideData} title="Classics"></CollectionList> */}
       <h1>WORK IN PROGRESS</h1>
     </Layout>
   );
