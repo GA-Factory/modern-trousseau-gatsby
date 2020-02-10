abcfbf957c9a29d921876d356fd7c43242cf79b8
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-02-10 09:46:04 -0500

    few components added in

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Layout from '../global/Layout';
-import Image from '../components/Image';
-import PrimaryPink from '../styles/styled-components'
-
+import Tagline from '../components/Tagline/tagline';
+import Footer from '../components/Footer/Footer';
 
 const Page = styled.div`
-  width: 100%;
+  width: 1440px;
+  max-width: 100%;
   height: 100vh;
   padding: 20px;
   display: flex;
@@ -16,27 +16,13 @@ const Page = styled.div`
   align-items: center;
 `;
 
-const Heading = styled.h1`
-  font-size: 24px;
-  color: #555;
-  margin-top: 60px;
-`;
 
-const Label = styled.p`
-  font-size: 14px;
-  color: #aaa;
-  margin-top: 12px;
-  letter-spacing: 10px;
-  text-transform: uppercase;
-`;
-
-const IndexPage = () => (
+const IndexPage = (props) => (
   <Layout>
     <Page>
-      <Image />
-      <Heading>GatsbyJS + Storybook: YAY</Heading>
-      <Label>It Works</Label>
-    </Page>
+      <Tagline headline="Classic Couture. Custom Fit." slogan="Where customization meets timeline meets budget." />
+      <Footer />
+      </Page>
   </Layout>
 );
 
