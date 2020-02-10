b3f49d6c2eebeb22d12467c31442cee7fd3a04ed
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-02-10 14:04:11 -0500

    working on styling header box

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
-import '../styles/fonts.css'
+import React from "react";
+import styled from "styled-components";
+import "../styles/fonts.css";
 
-import Layout from '../global/Layout';
-import Tagline from '../components/Tagline/tagline';
-import Footer from '../components/Footer/Footer';
-import Button from '../components/Button/Button'
+import Layout from "../global/Layout";
+import Logo from "../components/FullLogo/Logo";
+import Tagline from "../components/Tagline/tagline";
+import HowWeWork from "../components/HowWeWork/howwework";
+import OwnerQuote from "../components/owner-quote/owner-quote";
+import BrideReview from "../components/BrideReview/BrideReview";
+import Footer from "../components/Footer/Footer";
 
-const Page = styled.div`
-  width: 1440px;
-  max-width: 100%;
-  height: 100vh;
-  padding: 20px;
+// import imageFile1 from "../../assets/images/bridal-review-1.png";
+// import imageFile2 from "../../assets/images/bridal-review-2.jpeg";
+// import imageFile3 from "../assets/images/bridal-review-3.jpeg";
+
+// const images = [imageFile1, imageFile2, imageFile3];
+
+
+const HeaderBox = styled.div`
+  width: 100%;
+  background-color: #ffffff;
+  margin: 0 auto;
+  padding: 25px 0 0;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  flex-flow: row;
 `;
 
-
-const IndexPage = (props) => (
+const IndexPage = props => (
   <Layout>
-    <Page>
-      <Tagline headline="Classic Couture. Custom Fit." slogan="Where customization meets timeline meets budget." />
+      <PinkBox>
+      <Logo notag />
+      </PinkBox>
+      <Tagline
+        headline="Classic Couture. Custom Fit."
+        slogan="Where customization meets timeline meets budget."
+      />
+      <HowWeWork title="How We Work" />
+      <OwnerQuote
+        quote="The look on a bride's face when she finds her dress - that is priceless
+  and incredibly rewarding."
+        name="Callie Tein"
+        role="Designer"
+      />
+      {/* <BrideReview images={images} title="Fall 2020 Collection" /> */}
       <Footer />
-      </Page>
   </Layout>
 );
 
