fa5a03abf107672c1612937ff671a8cc14748cef
Mamady Sillah <supurpisces310@msn.com>
2020-02-10 11:04:15 -0500

    created desktop version 'Fall 2020 Collection' collection header

---
+++ src/components/CollectionHeaderDesk/CollectionHeaderDesk.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../CollectionHeaderDesk/CollectionHeaderDesk.css"
+
+
+
+class CollectionHeaderDesk extends React.Component {
+    
+   render() {
+       
+      return ( 
+         <div className="collection-header-desk"> 
+          <p>Fall 2020 Collection</p>
+          </div>
+      )
+   }
+}
+
+export default CollectionHeaderDesk;
---
+++ src/components/CollectionHeaderDesk/CollectionHeaderDesk.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import CollectionHeaderDesk from "../CollectionHeaderDesk/CollectionHeaderDesk"
+
+storiesOf('CollectionHeaderDesk', module)
+.add('CollectionHeaderDesk', () => <CollectionHeaderDesk></CollectionHeaderDesk>)
