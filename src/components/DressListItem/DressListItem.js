d467db5e0ded7ec33c51cb1f2ab62293940bb598
Igor Slabykh <igor.slabykh@gmail.com>
2020-02-11 16:08:40 -0500

    passed image and name as props

--- src/components/DressListItem/DressListItem.js
+++ src/components/DressListItem/DressListItem.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./DressListItem.css";
-import imageFile1 from "../../assets/images/seraphina.png";
 
-export default () => (
+const DressListItem = props => (
   <div className="one-item">
     <div className="dress-list-item-container">
-      <img src={imageFile1} alt="image" />
+      <img src={props.image} alt="image" />
     </div>
     <div>
-      <h5 className="dress-name">{"Seraphina"}</h5>
+      <h5 className="dress-name">{props.dressname}</h5>
     </div>
   </div>
 );
+
+export default DressListItem;
--- src/components/DressListItem/DressListItem.stories.js
+++ src/components/DressListItem/DressListItem.stories.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import imageFile1 from "../../assets/images/seraphina.png";
-
 import DressListItem from "./DressListItem";
 
 storiesOf("DressListItem", module).add("DressListItem", () => (
-  <DressListItem image1={imageFile1} dressname={"Seraphina"} />
+  <DressListItem image={imageFile1} dressname={"Seraphina"} />
 ));
