acbd4c3474e7cdfa78ec6cb1f13e4dc68650c99f
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-12 13:48:04 -0500

    fixed conflic ts

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -7,21 +7,6 @@ import instagram from "../Icons/instagram.png";
 import pinterest from "../Icons/pinterest.png";
 
 const SocialMedia = () => {
-<<<<<<< HEAD
-  return (
-    <>
-      <div class="con">
-        <h5>Stay Connected</h5>
-        <div class="some">
-          <img id="fb" src={facebook}></img>
-          <img id="in" src={instagram}></img>
-          <img id="pi" src={pinterest}></img>
-        </div>
-      </div>
-    </>
-  );
-};
-=======
     return (
         <>
             <div class="con">
@@ -35,6 +20,5 @@ const SocialMedia = () => {
         </>
     )
 }
->>>>>>> 2504b42ef1ef26f8f2364e26513c3e25bbf2408f
 
 export default SocialMedia;
