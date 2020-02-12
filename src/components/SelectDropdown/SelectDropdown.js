317342bcf931e8c21fd7f00b359216ba88860000
Adam_Bates <Creizai@EMAIL.COM>
2020-02-12 16:33:17 -0500

    Added up and down chevrons and tightened up the css media queriesgit add .! Looks to be all done

--- src/components/SelectDropdown/SelectDropdown.js
+++ src/components/SelectDropdown/SelectDropdown.js
@@ -1,23 +1,74 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import Icons from "../Icons/FAIcon";
 
-const DropdownSection = styled.div``;
+const DropdownSection = styled.div`
+  font-family: "";
+  background-color: ${props => props.theme.colors.backgroundGray};
+  color: ${props => props.theme.colors.nearBlack};
+`;
+
+const SelectionDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const UpDownChevronBox = styled.div`
+  flex-direction: column;
+  padding: 0;
+  margin: 0;
+`;
+
+const Chevrons = styled(Icons)`
+  width: 5px;
+`;
 
 const Selected = styled.ul`
+  width: 184px;
+  padding: 0 5px;
   position: relative;
-  padding: 5px;
   &:first-child {
-    font-weight: bold;
     border: 1px black solid;
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 16px;
+  }
+
+  @media only screen and (min-width: 600px) {
+    max-width: 410px;
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 800;
+    font-size: 21px;
+    line-height: 25px;
   }
 `;
 
 const Items = styled.li`
   z-index: 1;
   padding: 5px;
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 22px;
 
   &:hover {
-    background-color: pink;
+    color: white;
+    background-color: ${props => props.theme.colors.darkGray};
+  }
+
+  @media only screen and (min-width: 600px) {
+    max-width: 410px;
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 16px;
   }
 `;
 
@@ -62,7 +113,16 @@ class SelectDropdown extends Component {
     return (
       <DropdownSection>
         <ul>
-          <Selected onClick={this.toggle}>{this.state.title}</Selected>
+          <Selected onClick={this.toggle}>
+            <SelectionDiv>
+              {this.state.title}
+              <UpDownChevronBox>
+                <Chevrons name={["fas", "chevron-up"]} size="xs" />
+                <Chevrons name={["fas", "chevron-down"]} size="xs" />
+              </UpDownChevronBox>
+            </SelectionDiv>
+          </Selected>
+
           {this.state.active === true && Dropdown}
         </ul>
       </DropdownSection>
