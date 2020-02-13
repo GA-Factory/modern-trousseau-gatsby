89ebd82717d27e9449e9df644ee65f4dac715517
Igor Slabykh <igor.slabykh@gmail.com>
2020-02-12 22:46:20 -0500

    added a draft of styled components. Looks good but still need to work with it more

--- src/components/DressListItem/DressListItem.js
+++ src/components/DressListItem/DressListItem.js
@@ -1,15 +1,41 @@
 import React from "react";
-import "./DressListItem.css";
+// import "./DressListItem.css";
+import styled from "styled-components";
+
+const Image = styled.img`
+  margin: 0px;
+  padding: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  @media (max-width: 600px) {
+    width: 100%;
+    height: 100%;
+    background: #fae5dc;
+  }
+`;
+
+const DesktopText = styled.p`
+  text-align: center;
+  margin-top: 5px;
+  padding-bottom: 15px;
+  font-weight: 800;
+  font-family: ${props => props.theme.fontStyles.h3Styles.fontStyle};
+  font-size: ${props => props.theme.fontStyles.h3Styles.fontSize};
+`;
+
+const Wrapper = styled.div`
+@media (max-width: 600px) {
+ 
+  background: #fae5dc;
+
+`;
 
 const DressListItem = props => (
-  <div className="one-item">
-    <div className="dress-list-item-container">
-      <img src={props.image} alt="image" />
-    </div>
-    <div>
-      <h5 className="dress-name">{props.dressname}</h5>
-    </div>
-  </div>
+  <Wrapper>
+    <Image src={props.image} alt="image"></Image>
+
+    <DesktopText>{props.dressname}</DesktopText>
+  </Wrapper>
 );
 
 export default DressListItem;
