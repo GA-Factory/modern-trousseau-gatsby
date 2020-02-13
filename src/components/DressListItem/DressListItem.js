8737ede53946461dc03055a26bea3e22067f421b
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-13 11:57:08 -0500

    started making a layout and getting components in place

--- src/components/DressListItem/DressListItem.js
+++ src/components/DressListItem/DressListItem.js
@@ -5,6 +5,9 @@ const Image = styled.img`
   padding: 10px;
   margin-left: auto;
   margin-right: auto;
+  width: 100%;
+height: auto;
+
 `;
 
 const DesktopText = styled.p`
@@ -20,6 +23,8 @@ const DesktopText = styled.p`
 `;
 
 const Wrapper = styled.div`
+width: 100%;
+height: auto;
   @media (max-width: ${props => props.theme.breakpoints.tablet}) {
     background: #fae5dc;
   }
