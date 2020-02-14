97980a5ac515181dfbf295ca2361a1b3605e5312
Adam_Bates <Creizai@EMAIL.COM>
2020-02-14 16:46:29 -0500

    added the functionality to add to format the phone number poperly

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { number } from "prop-types";
 
 const Container = styled.div`
   max-width: 200px;
@@ -9,31 +10,44 @@ const Container = styled.div`
   font-size: 12px;
   line-height: 14px;
   display: flex;
+  flex-direction: column;
   align-items: center;
   text-align: center;
 `;
 
+const StoreInfo = styled.ul`
+  margin-top: 24px;
+`;
+
+const InfoItem = styled.li`
+  margin: 1px 0px;
+`;
 const Store = props => {
-  const StoreIndex = {};
+  // Adds ()  - - to the phone number
+  let num = props.phone;
+  var newNum =
+    "(" + num.slice(0, 3) + ")-" + num.slice(3, 6) + "-" + num.slice(6, 10);
+
   return (
     <>
       <Container>
-        <ul>
-          <li>{props.name}</li>
-          <li>{props.city}</li>
-          <li>{props.state}</li>
-          <li>{props.zip}</li>
-          <li>{props.phone}</li>
-          <li>{props.email}</li>
-          <li>{props.image}</li>
-          <li>{props.description}</li>
-          <li>{props.hours}</li>
-          {props.twitter != null && <li>`Twitter: {props.twitter}`</li>}
-          {props.instagram != null && <li>`Instagram: {props.instagram}`</li>}
-        </ul>
+        <img src={props.image} />
+        <StoreInfo>
+          <InfoItem>{props.name}</InfoItem>
+          <InfoItem>{props.street}</InfoItem>
+          <InfoItem>
+            {props.city}, {props.state} {props.zip}
+          </InfoItem>
+          <InfoItem>{newNum}</InfoItem>
+          <InfoItem>{props.email}</InfoItem>
+        </StoreInfo>
       </Container>
     </>
   );
 };
 
 export default Store;
+
+// Location index
+// {props.twitter != null && <li>`Twitter: {props.twitter}`</li>}
+// {props.instagram != null && <li>`Instagram: {props.instagram}`</li>}
