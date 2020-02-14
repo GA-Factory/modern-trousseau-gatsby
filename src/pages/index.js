196721fad2d3f024ff6cbddaba4b25a3e01ae5c9
John Serrao <bigbadserrao@gmail.com>
2020-02-14 17:04:39 -0500

    merging 169 - slider from dilmurod

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import { graphql } from "gatsby";
+import Image from "gatsby-image";
+import styled from "styled-components";
 
 import Layout from '../global/Layout';
 import Image from '../components/Image';
 import Logo from '../components/FullLogo/Logo';
 
+// const Page = styled.div`
+//   width: 100%;
+//   height: 100vh;
+//   padding: 20px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// `;
 
-const Page = styled.div`
-  width: 100%;
-  height: 100vh;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
+// const Heading = styled.h1`
+//   font-size: 24px;
+//   color: #555;
+//   margin-top: 60px;
+// `;
 
-const Heading = styled.h1`
-  font-size: 24px;
-  color: #555;
-  margin-top: 60px;
-`;
+// const Label = styled.p`
+//   font-size: 14px;
+//   color: #aaa;
+//   margin-top: 12px;
+//   letter-spacing: 10px;
+//   text-transform: uppercase;
+// `;
 
-const Label = styled.p`
-  font-size: 14px;
-  color: #aaa;
-  margin-top: 12px;
-  letter-spacing: 10px;
-  text-transform: uppercase;
-`;
-
-const IndexPage = () => (
+const IndexPage = props => (
   <Layout>
     <Page>
       <Image />
@@ -42,3 +43,19 @@ const IndexPage = () => (
 );
 
 export default IndexPage;
+
+export const sliderImageQuery = graphql`
+  query {
+    sliderImages: allFile(filter: { extension: { eq: "jpg" } }) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 800, quality: 90) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
