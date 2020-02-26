db8c41f1f9c4bb98731cb6038c40d061fe37d8a0
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-26 15:33:19 -0500

    slider works both ways and looks good.  flipping mobile and dektop code next

--- src/components/CollectionList/CollectionList.js
+++ src/components/CollectionList/CollectionList.js
@@ -45,34 +45,11 @@ const Button = styled.div`
   border: 0;
   margin: 0;
   font-size: 20px;
-  height: 276px;
+  height: 100%;
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     display: none;
   }
 `;
-
-// const ButtonA = styled.div`
-//   background-color: ${props => props.theme.colors.primaryPink};
-//   border: 0;
-//   margin: 0;
-//   font-size: 20px;
-//   height: 276px;
-//   padding-right: 12px;
-//   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-//     display: none;
-//   }
-// `;
-
-// const ButtonB = styled.div`
-//   background-color: ${props => props.theme.colors.primaryPink};
-//   border: 0;
-//   margin: 0;
-//   font-size: 20px;
-//   height: 276px;
-//   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-//     display: none;
-//   }
-// `;
 
 const ImageContainer = styled.div`
   display: flex;
@@ -83,11 +60,9 @@ const ImageContainer = styled.div`
 `;
 
 const SlideImage = styled.img`
-  height: 110%;
-  width: 90%;
-  margin: 3px;
+  height: auto;
+  width: 100%;
   margin-bottom: 0;
-  /* height: 100%; */
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     display: none;
   }
@@ -227,35 +202,58 @@ class CollectionList extends React.Component {
   nextProperty = () => {
     if (
       this.state.propertyA.index != this.props.slideData.length &&
-      this.state.propertyB.index != this.props.slideData.length
+      this.state.propertyB.index != this.props.slideData.length &&
+      this.state.propertyC.index != this.props.slideData.length
     ) {
       const newIndexA = this.state.propertyA.index + 1;
       const newIndexB = this.state.propertyB.index + 1;
+      const newIndexC = this.state.propertyC.index + 1;
       this.setState({
         propertyA: this.props.slideData[newIndexA],
-        propertyB: this.props.slideData[newIndexB]
+        propertyB: this.props.slideData[newIndexB],
+        propertyC: this.props.slideData[newIndexC]
       });
     }
     if (
       this.state.propertyA.index != this.props.slideData.length - 1 &&
-      this.state.propertyB.index == this.props.slideData.length - 1
+      this.state.propertyB.index != this.props.slideData.length - 1 &&
+      this.state.propertyC.index == this.props.slideData.length - 1
+    ) {
+      const newIndexA = this.state.propertyA.index + 1;
+      const newIndexB = this.state.propertyB.index + 1;
+      const newIndexC = 0;
+      this.setState({
+        propertyA: this.props.slideData[newIndexA],
+        propertyB: this.props.slideData[newIndexB],
+        propertyC: this.props.slideData[newIndexC]
+      });
+    }
+    if (
+      this.state.propertyA.index != this.props.slideData.length - 1 &&
+      this.state.propertyB.index == this.props.slideData.length - 1 &&
+      this.state.propertyC.index != this.props.slideData.length - 1
     ) {
       const newIndexA = this.state.propertyA.index + 1;
       const newIndexB = 0;
+      const newIndexC = this.state.propertyC.index + 1;
       this.setState({
         propertyA: this.props.slideData[newIndexA],
-        propertyB: this.props.slideData[newIndexB]
+        propertyB: this.props.slideData[newIndexB],
+        propertyC: this.props.slideData[newIndexC]
       });
     }
     if (
       this.state.propertyA.index == this.props.slideData.length - 1 &&
-      this.state.propertyB.index != this.props.slideData.length - 1
+      this.state.propertyB.index != this.props.slideData.length - 1 &&
+      this.state.propertyC.index != this.props.slideData.length - 1
     ) {
       const newIndexA = 0;
       const newIndexB = this.state.propertyB.index + 1;
+      const newIndexC = this.state.propertyC.index + 1;
       this.setState({
         propertyA: this.props.slideData[newIndexA],
-        propertyB: this.props.slideData[newIndexB]
+        propertyB: this.props.slideData[newIndexB],
+        propertyC: this.props.slideData[newIndexC]
       });
     }
   };
@@ -268,9 +266,9 @@ class CollectionList extends React.Component {
         <Button onClick={() => this.prevProperty()}>
         <ButtonIcon>&#10094;</ButtonIcon>
           </Button>
-          <SliderAPlacer>
+          {/* <SliderAPlacer> */}
             <SliderA propertyA={propertyA} />
-          </SliderAPlacer>
+          {/* </SliderAPlacer> */}
           <SliderB propertyB={propertyB} />
           <SliderC propertyC={propertyC} />
           <Button onClick={() => this.nextProperty()}>
