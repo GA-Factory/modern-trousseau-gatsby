69358babd0576ac931a36894156a2fb5d90b5456
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-02-28 16:11:40 -0500

    fixed padding issues

--- src/components/component-collection/ComponentCollection.js
+++ src/components/component-collection/ComponentCollection.js
@@ -8,9 +8,11 @@ const Collection = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
+  box-sizing: border-box;
+  padding: 25px 10px 25px 10px;
   background-color: ${props => props.theme.colors.primaryPink};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
-    height: 360px;
+    max-height: 380px;
     padding: 35px 10px 35px 10px;
   }
 `;
@@ -19,7 +21,7 @@ const Container = styled.div`
   display: flex;
   width: 100%;
   height: auto;
-  padding: 25px 10px 20px;
+  padding: 5px 0px 15px;
   justify-content: space-evenly;
   background-color: ${props => props.theme.colors.primaryPink};
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
@@ -71,14 +73,18 @@ const GownImageScreen2 = styled.img`
   }
 `;
 
-const Title = styled.p`
+const Title = styled.h3`
   text-align: center;
   font-weight: bold;
-  font-family: ${props => props.theme.fontStyles.h1Styles.fontStyle};
+  font-family: 'Playfair Display', serif;
+  font-size: 18px;
+  line-height: 19px;
+  color: #0F1F2F;
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     font-family: ${props => props.theme.fontStyles.h3Styles.fontStyle};
     font-size: 24px;
     margin-top: 10px;
+    font-family: "Raleway", sans-serif;
   }
 `;
 
