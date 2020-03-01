cdc971a27ce0dd5cad272f18cc9b0234f81bcca0
Kelly M Collins <90kcol@gmail.com>
2020-03-01 09:34:10 -0500

    fixed styled components on NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,50 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled from 'styled-components';
 import Icons from "../Icons/FAIcon";
-import "./NavBar.css";
 import Logo from "../FullLogo/Logo";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  margin: 0 auto;
+  align-items: center;
+  background-color: #E5E5E5;
+  padding: 10px 20px;
+  @media screen and (max-width: 768px) {
+    justify-content: space-between;
+  }
+`;
+
+const Desktop = styled.div`
+  padding: 0px 10px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Mobile = styled.div`
+  display: none;
+  padding: 0px 10px;
+  @media screen and (max-width: 768px) {
+    padding-top: 15px;
+    display: block;
+  }
+`;
+
 
 const NavBar = props => {
   return (
     <>
-      <nav className="nav-bar">
-        <div className="mobile-style">
+      <Nav>
+        <Mobile>
           {/* Replace with burger component when complete */}
           <Link to="#">
             <Icons name={['fas', 'bars']} size="lg" />
           </Link>
         </Mobile>
         <Mobile>
-          {/* Replace with logo component */}
           <Logo notag tagline title1="MODERN" title2="trousseau" />
         </Mobile>
         <Mobile>
@@ -37,8 +66,8 @@ const NavBar = props => {
           <Link to="#">
             <Icons name={['far', 'user']} />
           </Link>
-        </div>
-      </nav>
+        </Desktop>
+      </Nav>
     </>
   );
 };
