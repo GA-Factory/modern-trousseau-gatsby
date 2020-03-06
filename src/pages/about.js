ffe361795a7120e48f9fdd7078554c78c72868f5
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-06 09:11:51 -0500

    created about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import HeaderNavBar from "../components/HeaderNavBar/HeaderNavBar";
+import Layout from '../global/Layout';
+
+const About = props => {
+    return (
+    <Layout>
+        <NavBar />
+        <HeaderNavBar />
+    </Layout>
+    )
+};
+
+export default About;
