adbdab67c02b58eb7cba266c2003d1788983340c
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-06 09:24:37 -0500

    created component in folder

---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const AboutSection = styled.section`
+  width: 100%;
+`;
+
+const AboutUs = props => {
+  return <AboutSection></AboutSection>;
+};
+
+export default AboutUs;
