232ac58914e291c534297690d71f8b487649cea5
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-06 10:39:29 -0500

    added in text from about

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -3,10 +3,65 @@ import styled from "styled-components";
 
 const AboutSection = styled.section`
   width: 100%;
+  background-color: #ffffff;
+  padding: 45px 0;
+  position: relative;
+  margin: 0 auto;
+  max-width: 950px;
+`;
+
+const AboutContainer = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  display: flex;
+  flex-flow: wrap;
+  padding: 15px 0 0 0;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  font-family: "Raleway", sans-serif;
+  font-size: 30px;
+  line-height: 42px;
+  text-transform: uppercase;
+  padding: 0;
+  margin: 0 auto;
+`;
+
+const Text = styled.p`
+  font-family: "Raleway", sans-serif;
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: normal;
+  letter-spacing: auto;
+  margin: 0 auto 1em;
 `;
 
 const AboutUs = props => {
-  return <AboutSection></AboutSection>;
+  return (
+    <AboutSection>
+      <Title>About Modern Trousseau</Title>
+      <AboutContainer>
+        <Text>
+          Callie’s designs feature fine French and Italian fabrics and exclusive
+          laces; when the perfect fabric cannot be found to suit her design, she
+          will create the lace patterns and beadwork for productions in the
+          finest ateliers. Modern Trousseau’s own production house is based in
+          Woodbridge, CT, meaning all gowns are designed and made with love in
+          the USA.
+        </Text>
+        <Text>
+          Since the label launched in 2004, Modern Trousseau has been cemented
+          as a favorite amongst brides worldwide. Callie’s distinctive designs
+          have been featured in many print and online publications, including
+          Inside Weddings, People Magazine, Brides, Martha Stewart Weddings, The
+          Knot and Style Me Pretty.
+        </Text>
+        <Text>The hallmark of the Modern Trousseau label is customization; we are proud to offer endless modification options to suit a bride’s individual vision for her wedding day look. Combine tops and bottoms, change necklines, modify shapes, choose colors, swap out laces and embroidery, enhance beading, add embellishments, and much more. All of these options to create a custom bridal gown, along with a quality fit, make each Modern Trousseau gown a one-of-a-kind, wearable work of art.</Text>
+        <Text>Modern Trousseau bridal gowns are designed and made with love in the USA, and available at our flagship boutiques and exclusive retailers worldwide.</Text>
+      </AboutContainer>
+    </AboutSection>
+  );
 };
 
 export default AboutUs;
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import OwnerQuote from "../components/owner-quote/owner-quote";
 import ComponentCollection from "../components/component-collection/ComponentCollection";
 import BrideReview from "../components/BrideReview/BrideReview";
 import Footer from "../components/Footer/Footer";
+import AboutUs from "../components/AboutUs/AboutUs"
 
 const ImageQuery = styled(Img)`
   display: block;
@@ -78,6 +79,7 @@ const IndexPage = props => {
         title={props.data.contentfulCollection.collectionName}
         data={props.data}
       ></ComponentCollection>
+      <AboutUs />
       <BrideReview
         data={props.data}
         image1={props.data.allContentfulTestimonial.nodes[0].image.fluid}
