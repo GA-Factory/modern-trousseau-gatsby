d76895452cee9f8ef2b0ba06402fb5cc4926527b
Igor Slabykh <igor.slabykh@gmail.com>
2020-03-09 23:46:54 -0400

    changed styling to get rid of gray bar on the top. There is still gray bar onthe top in mobile version. Changed hamburger menu icon on the gray bar to location icon

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,12 +8,14 @@ const NavBar = props => {
   return (
     <>
       <nav className="nav-bar">
+        {/* gray menu starts */}
         <div className="mobile-style">
           {/* Replace with burger component when complete */}
           <Link to="#">
-            <Icons name={["fas", "bars"]} size="lg" />
+            <Icons name={["fas", "map-marker-alt"]} size="lg" />
           </Link>
         </div>
+        {/* gray menu ends */}
         <div className="mobile-style">
           {/* Replace with logo component */}
           <Logo notag tagline title1="MODERN" title2="trousseau" />
