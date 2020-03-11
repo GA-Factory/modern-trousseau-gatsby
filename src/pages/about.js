d73b999048e8650c90badef786e2e2953789e2a2
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-11 13:06:36 -0400

    added in image needed just waiting for it to update to query in page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,12 @@ import React from 'react';
 import NavBar from '../components/NavBar/NavBar';
 import HeaderNavBar from "../components/HeaderNavBar/HeaderNavBar";
 import Layout from '../global/Layout';
+import Img from "gatsby-image";
+import { useStaticQuery, graphql } from "gatsby";
 
 const About = props => {
+    // const aboutinfo = useStaticQuery(graphql`
+    // `);
     return (
     <Layout>
         <NavBar />
