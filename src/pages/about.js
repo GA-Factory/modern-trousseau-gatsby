cd7e0baf3a726b4124c87a1418857f3db679584c
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-11 13:26:46 -0400

    added in guery for image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import NavBar from '../components/NavBar/NavBar';
+import React from "react";
+import NavBar from "../components/NavBar/NavBar";
 import HeaderNavBar from "../components/HeaderNavBar/HeaderNavBar";
-import Layout from '../global/Layout';
-import Footer from "../components/Footer/Footer"
+import Layout from "../global/Layout";
+import Footer from "../components/Footer/Footer";
 import Img from "gatsby-image";
 import { useStaticQuery, graphql } from "gatsby";
 
 const About = props => {
-    // const aboutinfo = useStaticQuery(graphql`
-    // `);
-    return (
+  const aboutInfo = useStaticQuery(graphql`
+    query AboutQuery {
+      contentfulAsset(file: { fileName: { eq: "Callie.jpg" } }) {
+        fluid {
+          base64
+          src
+          srcSet
+          srcSetWebp
+          srcWebp
+        }
+        file {
+          fileName
+        }
+      }
+    }
+  `);
+  return (
     <Layout>
-        <NavBar />
-        <HeaderNavBar />
-        <Footer />
+      <NavBar />
+      <HeaderNavBar />
+      <Footer />
     </Layout>
-    )
+  );
 };
 
 export default About;
