ab73f0be2b19e4a1263c3252dc52425b12adbafb
Igor Slabykh <igor.slabykh@gmail.com>
2020-03-16 23:07:46 -0400

    commented out hamburger menu and started creating a new one from the scrach

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,48 +1,59 @@
 import React, { Component } from "react";
-import FAIcon from "../Icons/FAIcon";
-import "./Hamburger.css";
+import styled from "styled-components";
 
-class Burger extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-      isClosed: true
-    };
-  }
+const MenuIcon = styled.button``;
 
-  toggleMenu() {
-    this.setState({
-      isOpen: !this.state.isOpen,
-      isClosed: !this.state.isClosed
-    });
-  }
-
-  render() {
-    return (
-      <div className="wrapper">
-        <button onClick={() => this.toggleMenu()}>
-          {this.state.isClosed && (
-            <FAIcon
-              className="menu-toggle bars"
-              name={["fas", "bars"]}
-              size="lg"
-            />
-          )}
-          {this.state.isOpen && (
-            <FAIcon
-              className="menu-toggle cross"
-              name={["fas", "times"]}
-              size="lg"
-            />
-          )}
-        </button>
-        {this.state.isOpen && <div className="Menu">{this.props.menu1}</div>}
-        {this.state.isOpen && <div className="Menu">{this.props.menu2}</div>}
-        {this.state.isOpen && <div className="Menu">{this.props.menu3}</div>}
-      </div>
-    );
-  }
-}
+const Burger = () => {
+  return <div>test</div>;
+};
 
 export default Burger;
+
+// import React, { Component } from "react";
+// import FAIcon from "../Icons/FAIcon";
+// import "./Hamburger.css";
+
+// class Burger extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isOpen: false,
+//       isClosed: true
+//     };
+//   }
+
+//   toggleMenu() {
+//     this.setState({
+//       isOpen: !this.state.isOpen,
+//       isClosed: !this.state.isClosed
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="wrapper">
+//         <button onClick={() => this.toggleMenu()}>
+//           {this.state.isClosed && (
+//             <FAIcon
+//               className="menu-toggle bars"
+//               name={["fas", "bars"]}
+//               size="lg"
+//             />
+//           )}
+//           {this.state.isOpen && (
+//             <FAIcon
+//               className="menu-toggle cross"
+//               name={["fas", "times"]}
+//               size="lg"
+//             />
+//           )}
+//         </button>
+//         {this.state.isOpen && <div className="Menu">{this.props.menu1}</div>}
+//         {this.state.isOpen && <div className="Menu">{this.props.menu2}</div>}
+//         {this.state.isOpen && <div className="Menu">{this.props.menu3}</div>}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Burger;
