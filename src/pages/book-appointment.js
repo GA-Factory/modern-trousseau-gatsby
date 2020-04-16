f3c33181be7647e04d0b79efccafcfe54c665008
Igor Slabykh <igor.slabykh@gmail.com>
2020-04-15 22:20:59 -0400

    changed the name of the page and added Link to header nav bar

--- src/pages/book-appointment.js
+++ src/pages/book-appointment.js
@@ -4,3 +4,12 @@ import Layout from "../global/Layout";
 import NavBar from "../components/NavBar/NavBar";
 import HeaderNavBar from "../components/HeaderNavBar/HeaderNavBar";
 import Footer from "../components/Footer/Footer";
+import styled from "styled-components";
+
+const AppointmentPage = () => (
+  <Layout>
+    <Footer />
+  </Layout>
+);
+
+export default AppointmentPage;
