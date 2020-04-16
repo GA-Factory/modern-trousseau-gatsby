ff14cfda17610c9121ff439fec961b8ce73505d7
Igor Slabykh <igor.slabykh@gmail.com>
2020-04-15 22:32:32 -0400

    added header and footer to booking page

--- src/pages/book-appointment.js
+++ src/pages/book-appointment.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
 import Layout from "../global/Layout";
 import NavBar from "../components/NavBar/NavBar";
 import HeaderNavBar from "../components/HeaderNavBar/HeaderNavBar";
 import Footer from "../components/Footer/Footer";
+
 import styled from "styled-components";
 
 const AppointmentPage = () => (
   <Layout>
+    <HeaderNavBar />
+    <NavBar />
+    <h1>book-appointment page. this sign is for testing purposes</h1>
     <Footer />
   </Layout>
 );
