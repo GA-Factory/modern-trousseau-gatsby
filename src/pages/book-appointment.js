30c7018b7fa671b0a3005ecd87a62f8e1da8cd6f
Igor Slabykh <igor.slabykh@gmail.com>
2020-04-22 18:27:30 -0400

    last Igor's commitment

--- src/pages/book-appointment.js
+++ src/pages/book-appointment.js
@@ -3,14 +3,27 @@ import Layout from "../global/Layout";
 import NavBar from "../components/NavBar/NavBar";
 import HeaderNavBar from "../components/HeaderNavBar/HeaderNavBar";
 import Footer from "../components/Footer/Footer";
+import SelectDropdown from "../components/SelectDropdown/SelectDropdown";
+import TextInput from "../components/TextInput/TextInput";
 
 import styled from "styled-components";
 
-const AppointmentPage = () => (
+const AppointmentPage = props => (
   <Layout>
     <HeaderNavBar />
     <NavBar />
-    <h1>book-appointment page. this sign is for testing purposes</h1>
+    <p>book-appointment page. this sign is for testing purposes</p>
+    {/* <SelectDropdown /> does not work */}
+    <TextInput />
+    <form
+      name="first name"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input name="first name" placeholder="First name" type="text" />
+      <button>NAme</button>
+    </form>
     <Footer />
   </Layout>
 );
