04a756e5adff3bcb0eed70d818fd6a79e712117f
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-04-27 22:15:02 -0400

    working on getting my GraphQL query for the collection page

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -15,6 +15,46 @@ import { Link } from "gatsby";
 
 // import cardData from "./Gowns"
 
+export const GownQuery = graphql`
+  query MyQuery {
+    allContentfulGowns(limit: 40, sort: { fields: name }) {
+      edges {
+        node {
+          collections {
+            collectionName
+            gowns {
+              name
+              gownImage {
+                fluid {
+                  base64
+                  src
+                  srcSet
+                  srcSetWebp
+                  srcWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    allContentfulCollection(limit: 40) {
+      nodes {
+        collectionName
+        gowns {
+          name
+          gownImage {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+
 import imageFile1 from "../assets/images/Fall-2020-1.png";
 import imageFile2 from "../assets/images/Fall-2020-2.png";
 import imageFile3 from "../assets/images/Spring-2020-1.jpeg";
@@ -26,56 +66,56 @@ import imageFile8 from "../assets/images/Classics-2.jpeg";
 
 
 
-const cardData = [
-  {
-    image: imageFile1,
-    mobileLabel: "LUCA",
-    desktopLabel: "Luca",
-    index: 0
-  },
-  {
-    image: imageFile2,
-    mobileLabel: "VIDA",
-    desktopLabel: "Vida",
-    index: 1
-  },
-  {
-    image: imageFile3,
-    mobileLabel: "ALWAYS",
-    desktopLabel: "Always",
-    index: 2
-  },
-  {
-    image: imageFile4,
-    mobileLabel: "BELIZE",
-    desktopLabel: "Belize",
-    index: 3
-  },
-  {
-    image: imageFile5,
-    mobileLabel: "ALICE",
-    desktopLabel: "Alice",
-    index: 4
-  },
-  {
-    image: imageFile6,
-    mobileLabel: "EVELYN",
-    desktopLabel: "Evelyn",
-    index: 5
-  },
-  {
-    image: imageFile7,
-    mobileLabel: "AINSLEY",
-    desktopLabel: "Ainsley",
-    index: 6
-  },
-  {
-    image: imageFile8,
-    mobileLabel: "ANNIE",
-    desktopLabel: "Annie",
-    index: 7
-  }
-];
+// const cardData = [
+//   {
+//     image: imageFile1,
+//     mobileLabel: "LUCA",
+//     desktopLabel: "Luca",
+//     index: 0
+//   },
+//   {
+//     image: imageFile2,
+//     mobileLabel: "VIDA",
+//     desktopLabel: "Vida",
+//     index: 1
+//   },
+//   {
+//     image: imageFile3,
+//     mobileLabel: "ALWAYS",
+//     desktopLabel: "Always",
+//     index: 2
+//   },
+//   {
+//     image: imageFile4,
+//     mobileLabel: "BELIZE",
+//     desktopLabel: "Belize",
+//     index: 3
+//   },
+//   {
+//     image: imageFile5,
+//     mobileLabel: "ALICE",
+//     desktopLabel: "Alice",
+//     index: 4
+//   },
+//   {
+//     image: imageFile6,
+//     mobileLabel: "EVELYN",
+//     desktopLabel: "Evelyn",
+//     index: 5
+//   },
+//   {
+//     image: imageFile7,
+//     mobileLabel: "AINSLEY",
+//     desktopLabel: "Ainsley",
+//     index: 6
+//   },
+//   {
+//     image: imageFile8,
+//     mobileLabel: "ANNIE",
+//     desktopLabel: "Annie",
+//     index: 7
+//   }
+// ];
 
 const collectionTitle = [
   "Fall 2020 Collection",
